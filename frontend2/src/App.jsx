e7bdfa0d979dce320b7caa7a6170cfbe26a913ff
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2024-12-28 19:14:54 +0545

    Page not found

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -10,13 +10,13 @@ const App = () => {
   return (
     <div>
       {/* {!excludeNavFooterRoutes.includes(Location.pathname) && <Navbar />} */}
-      <AppRoutes />
+      {/* <AppRoutes /> */}
       {/* {!excludeNavFooterRoutes.includes(Location.pathname) && <Footer />} */}
-      {/* <div className="h-screen flex justify-center items-center text-4xl font-[600] text-[#4f46e5]">
+      <div className="h-screen flex justify-center items-center text-4xl font-[600] text-[#4f46e5]">
         {"<Sorry>"} <br />
         &nbsp; &nbsp;&nbsp;&nbsp; Website is Under Construction <br />
         {"</Sorry>"}
-      </div> */}
+      </div>
 
       {/* <Home/>  */}
     </div>
